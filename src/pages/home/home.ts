4a5a3f30ec0414b9437014c3718450652dc4c01d
Omid Nowrouzi <onowrouzi@outlook.com>
2018-03-21 19:13:23 -0500

    Basic game list test.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+
+import { sortBy } from 'lodash';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  games: any[];
+  constructor(public navCtrl: NavController, private _http: HttpClient) {
+    _http.get('/api/games/ajax/filtered', {
+      params: {
+        mediaType: 'game',
+        sort: 'title'
+      }
+    }).subscribe((res:any) => this.games = res.products);
   }
 
 }
